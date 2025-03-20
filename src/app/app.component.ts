9f59c757cb6346d3079e6cca644366827cfce43d
Abijoe2004 <abijoe202004@gmail.com>
2025-03-21 00:47:50 +0530

    abijoe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OperationstatusComponent} from './operationstatus/operationstatus.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,OperationstatusComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'operationstatus';
+  title = 'form';
 }
